b21625a6518e967965d054ec98584b8c166975b3
TheSaibankan <51401079+TheSaibankan@users.noreply.github.com>
2020-06-23 10:07:20 +0200

    Initial commit

---
+++ Problems/Guest list of a hotel/src/Main.java
@@ -0,0 +1,32 @@
+//put imports you need here
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String duo1 = scanner.next();
+        String duo2 = scanner.next();
+
+        String solo = scanner.next();
+
+        String trio1 = scanner.next();
+        String trio2 = scanner.next();
+        String trio3 = scanner.next();
+
+        String duo3 = scanner.next();
+        String duo4 = scanner.next();
+
+
+
+       System.out.println(duo4);
+       System.out.println(duo3);
+       System.out.println(trio3);
+       System.out.println(trio2);
+       System.out.println(trio1);
+       System.out.println(solo);
+       System.out.println(duo2);
+       System.out.println(duo1);
+    }
+}
---
+++ Problems/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word1 = sc.next();
+        String word2 = sc.next();
+        String word3 = sc.next();
+
+        System.out.println(word3);
+        System.out.println(word2);
+        System.out.println(word1);
+    }
+}
